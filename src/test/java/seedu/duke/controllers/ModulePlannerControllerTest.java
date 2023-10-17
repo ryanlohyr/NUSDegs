a777fd584c0ae40bcd4d764c056dde18112af52d
SebasFok <sebasfok2001@gmail.com>
2023-10-17 19:50:39 +0800

    Add tests for 'updateMajor' function

--- src/test/java/seedu/duke/controllers/ModulePlannerControllerTest.java
+++ src/test/java/seedu/duke/controllers/ModulePlannerControllerTest.java
@@ -5,12 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import seedu.duke.ModuleList;
+import seedu.duke.models.Major;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class ModulePlannerControllerTest {
     private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
@@ -101,5 +105,27 @@ class ModulePlannerControllerTest {
         for (int i = 0; i < numberOfModules; i += 1) {
             assertEquals(actualDifference.getMainModuleList().get(i), list.get(i));
         }
+    }
+
+    //success scenario: valid major -> major updated
+    @Test
+    void updateMajor_validMajor_expectSuccessMessage() {
+        ModulePlannerController controller = new ModulePlannerController();
+        String major = "cs";
+        controller.updateMajor(major);
+        String printedOutput = outputStream.toString().trim();
+        assertEquals(String.format("Major %s selected!",major.toUpperCase()), printedOutput);
+    }
+
+    //failure scenario invalid major -> throw exception
+    @Test
+    void updateMajor_invalidMajor_expectFailureMessage() {
+        ModulePlannerController controller = new ModulePlannerController();
+        String major = "abc";
+        controller.updateMajor(major);
+        String printedOutput = outputStream.toString().trim();
+        assertEquals("Please select a major from this list: " + Arrays.toString(Major.values()),
+                    printedOutput);
+
     }
 }
