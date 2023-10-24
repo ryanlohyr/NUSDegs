a74c4c143857f517b7518f354aa339e5b90c9eb6
rohit <rohitdarnell@gmail.com>
2023-10-24 19:44:05 +0800

    Add API Class

--- src/test/java/seedu/duke/ApiTest.java
+++ src/test/java/seedu/duke/ApiTest.java
@@ -7,7 +7,6 @@ import java.net.URISyntaxException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApiTest {
