67ba22e5c71002d4c29b71c0e9bc4aa7e1c8f176
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-10-29 00:15:05 +0800

    Show half table

--- src/main/java/seedu/duke/views/CommandLineView.java
+++ src/main/java/seedu/duke/views/CommandLineView.java
@@ -3,9 +3,13 @@ package seedu.duke.views;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CommandLineView {
+    private String topline = "_____________________________________________________________________";
+    private String emptyline = "|___________________________________________________________________|";
+
     public void displayWelcome(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -25,9 +29,44 @@ public class CommandLineView {
 
         File f = new File(filePath); // create a File for the given file path
         Scanner s = new Scanner(f); // create a Scanner using the File as the source
+        boolean prevLineEndCell = false;
+
         while (s.hasNext()) {
             String currentLine = s.nextLine();
-            System.out.println("\t" + currentLine);
+            if (!currentLine.isEmpty()) {
+                if (currentLine.charAt(0) == '*') { //header
+                    if (currentLine.charAt(1) == '*') { //subheader {
+                        if (currentLine.charAt(2) == '*') { //subsubheader {
+                            //print box
+                            if (!prevLineEndCell) {
+                                System.out.println(topline);
+                            }
+                            System.out.println("|\n|\t" + currentLine.substring(3) + "\n" + emptyline);
+                            prevLineEndCell = true;
+                        } else {
+                            //print box
+                            if (!prevLineEndCell) {
+                                System.out.println(topline);
+                            }
+                            System.out.println("|\n|\t" + currentLine.substring(2) + "\n" + emptyline);
+                            prevLineEndCell = true;
+                        }
+                    } else {
+                        //print as per normal
+                        System.out.println("\t" + currentLine.substring(1));
+                        prevLineEndCell = false;
+                    }
+                } else {
+                    System.out.println("|\t" + currentLine);
+                    prevLineEndCell = false;
+                }
+            } else {
+                if (prevLineEndCell) {
+                    System.out.println("|");
+                }
+                System.out.println(emptyline + "\n");
+                prevLineEndCell = false;
+            }
         }
     }
 }
