b9fac348cbba2fe6641b522779774367f60831de
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-10-28 23:08:48 +0800

    Update getRequirements to get only module codes from full CEGRequirements.txt

--- src/main/java/seedu/duke/models/logic/DataRepository.java
+++ src/main/java/seedu/duke/models/logic/DataRepository.java
@@ -4,10 +4,11 @@ package seedu.duke.models.logic;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class DataRepository {
-    public static ArrayList<String> getRequirements(String major){
+    public static ArrayList<String> getFullRequirements(String major){
         try{
             //add validation if wrong major?
             String fileName = String.format("src/main/java/seedu/duke/models/data/%sRequirements",major);
@@ -21,6 +22,32 @@ public class DataRepository {
             while (s.hasNext()) {
                 String currentLine = s.nextLine();
                 currentArray.add(currentLine);
+            }
+            return currentArray;
+        }catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ArrayList<String> getRequirements(String major){
+        try{
+            //add validation if wrong major?
+            String fileName = String.format("src/main/java/seedu/duke/models/data/%sRequirements",major);
+            File f = new File(fileName);
+            if (!f.exists()) {
+                System.out.println("File does not exist");
+                return new ArrayList<>();
+            }
+            ArrayList<String> currentArray = new ArrayList<>();
+            Scanner s = new Scanner(f);
+            while (s.hasNext()) {
+                String currentLine = s.nextLine();
+
+                String[] words = currentLine.split(" ");
+
+                if (!words[0].isEmpty() && words[0].charAt(0) != '*') { // not empty line, not title
+                    currentArray.add(words[0]);
+                }
             }
             return currentArray;
         }catch (FileNotFoundException e) {
