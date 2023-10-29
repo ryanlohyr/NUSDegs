a457e05e8264244efecf3d6ea200b01d860c35a1
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-10-29 17:57:10 +0800

    Complete simple formatting for "required" feature

--- src/main/java/seedu/duke/views/CommandLineView.java
+++ src/main/java/seedu/duke/views/CommandLineView.java
@@ -2,13 +2,10 @@ package seedu.duke.views;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class CommandLineView {
-    private String topline = "_____________________________________________________________________";
-    private String emptyline = "|___________________________________________________________________|";
+    private int formatLineLength = 0;
 
     public void displayWelcome(){
         String logo = " ____        _        \n"
@@ -24,49 +21,79 @@ public class CommandLineView {
         System.out.println(message);
     }
 
+    public void displayMessage() {
+        System.out.println();
+    }
+
+    private void printTopLine() {
+        displayMessage(String.format("┌%-" + formatLineLength + "s┐", "").replace(' ', '—'));
+    }
+
+    private void printDoubleTopLine() {
+        displayMessage(String.format("╔%-" + formatLineLength + "s╗", "").replace(' ', '═'));
+    }
+
+    private void printBottomLine() {
+        displayMessage(String.format("└%-" + formatLineLength + "s┘", "").replace(' ', '—'));
+    }
+
+    private void printDoubleBottomLine() {
+        displayMessage(String.format("╚%-" + formatLineLength + "s╝", "").replace(' ', '═'));
+    }
+
+    private String returnJustified(String name, String description, int length) {
+        return String.format("%-" + length + "s\t", name) + description;
+    }
+
     public void printTXTFile(String filePath) throws FileNotFoundException {
         //ArrayList<Task> tasks = new ArrayList<>();
 
         File f = new File(filePath); // create a File for the given file path
-        Scanner s = new Scanner(f); // create a Scanner using the File as the source
-        boolean prevLineEndCell = false;
+        Scanner firstScanner = new Scanner(f); // create a Scanner using the File as the source
+        int longestStringLength = 0;
 
-        while (s.hasNext()) {
-            String currentLine = s.nextLine();
-            if (!currentLine.isEmpty()) {
-                if (currentLine.charAt(0) == '*') { //header
-                    if (currentLine.charAt(1) == '*') { //subheader {
-                        if (currentLine.charAt(2) == '*') { //subsubheader {
-                            //print box
-                            if (!prevLineEndCell) {
-                                System.out.println(topline);
-                            }
-                            System.out.println("|\n|\t" + currentLine.substring(3) + "\n" + emptyline);
-                            prevLineEndCell = true;
-                        } else {
-                            //print box
-                            if (!prevLineEndCell) {
-                                System.out.println(topline);
-                            }
-                            System.out.println("|\n|\t" + currentLine.substring(2) + "\n" + emptyline);
-                            prevLineEndCell = true;
-                        }
-                    } else {
-                        //print as per normal
-                        System.out.println("\t" + currentLine.substring(1));
-                        prevLineEndCell = false;
-                    }
-                } else {
-                    System.out.println("|\t" + currentLine);
-                    prevLineEndCell = false;
-                }
-            } else {
-                if (prevLineEndCell) {
-                    System.out.println("|");
-                }
-                System.out.println(emptyline + "\n");
-                prevLineEndCell = false;
+        while (firstScanner.hasNext()) {
+            String currentLine = firstScanner.nextLine();
+            if (currentLine.indexOf(" - ") > longestStringLength) {
+                longestStringLength = currentLine.indexOf(" - ");
             }
+        }
+        formatLineLength = longestStringLength + 15;
+
+        Scanner secondScanner = new Scanner(f);
+        String moduleName;
+        String moduleMCs;
+
+        while (secondScanner.hasNext()) {
+            String currentLine = secondScanner.nextLine();
+            if (currentLine.indexOf(" - ") > 0) { //module exist
+                moduleName = currentLine.substring(0, currentLine.indexOf(" - "));
+                moduleMCs = currentLine.substring(currentLine.indexOf(" - ") + 3);
+            } else {
+                moduleName = currentLine;
+                moduleMCs = "";
+            }
+
+            if (currentLine.startsWith("***")) { //subsubheader
+                String actualModuleName = moduleName.substring(3);
+                displayMessage(" ~~\t" + returnJustified(actualModuleName, moduleMCs, longestStringLength) + "\t ~~");
+                displayMessage();
+            } else if (currentLine.startsWith("**")) { //subheader has box
+                printTopLine();
+                String actualModuleName = moduleName.substring(2);
+                displayMessage("│\t" + returnJustified(actualModuleName, moduleMCs, longestStringLength) + "\t\t│");
+                printBottomLine();
+            } else if (currentLine.startsWith("*")) { //header
+                printDoubleTopLine();
+                String actualModuleName = moduleName.substring(1);
+                displayMessage("║\t" + returnJustified(actualModuleName, moduleMCs, longestStringLength) + "\t║");
+                printDoubleBottomLine();
+            } else if (!currentLine.isEmpty()) {
+                displayMessage("\t" + returnJustified(moduleName, moduleMCs, longestStringLength));
+            } else {
+                displayMessage();
+            }
+
         }
     }
 }
