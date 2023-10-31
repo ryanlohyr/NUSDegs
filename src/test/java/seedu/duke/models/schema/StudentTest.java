d504b38d405a6f65395c26b49fd08db0f46d2a3b
SebasFok <sebasfok2001@gmail.com>
2023-10-31 19:37:24 +0800

    Add tests for schedule class

---
+++ src/test/java/seedu/duke/models/schema/StudentTest.java
@@ -0,0 +1,10 @@
+package seedu.duke.models.schema;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.models.schema.Student;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StudentTest {
+
+}
