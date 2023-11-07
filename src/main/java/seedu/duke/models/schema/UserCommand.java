3ab8fa754858e0ae888b31cb2f992567b7e2007d
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-11-07 12:40:28 +0800

    UG edit fixes #90

---
+++ src/main/java/seedu/duke/models/schema/UserCommand.java
@@ -0,0 +1,134 @@
+package seedu.duke.models.schema;
+
+import seedu.duke.models.logic.Api;
+import seedu.duke.utils.Parser;
+import seedu.duke.utils.errors.UserError;
+
+import static seedu.duke.controllers.ModuleMethodsController.*;
+import static seedu.duke.controllers.ModuleMethodsController.getRequiredModulesForStudent;
+import static seedu.duke.utils.Parser.parseArguments;
+import static seedu.duke.utils.Parser.parseCommand;
+import static seedu.duke.views.CommandLineView.printListOfCommands;
+
+public class UserCommand implements UserCommandWord {
+
+    private String userInput;
+    private String commandWord;
+    private String[] arguments;
+    private boolean isValid;
+
+    private CommandManager commandManager;
+
+
+    public UserCommand(String userInput) {
+        this.commandManager = new CommandManager();
+        this.userInput = userInput;
+        commandWord = parseCommand(userInput);
+        arguments = parseArguments(userInput);
+        isValid = true;
+
+        if (!commandManager.getListOfCommandNames().contains(commandWord)){
+            UserError.displayInvalidInputCommand(commandWord);
+            isValid = false;
+        } else {
+
+            boolean validInput = Parser.isValidInputForCommand(commandWord, arguments);
+
+            if (!validInput) {
+                UserError.displayInvalidMethodCommand(commandWord);
+                isValid = false;
+
+            }
+        }
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public static String getUserInput() {
+        return userInput;
+    }
+
+    public static String getCommandWord() {
+        return commandWord;
+    }
+
+    public static String[] getArguments() {
+        return arguments;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+/*
+    public void processCommand(Student student) {
+        switch (commandWord) {
+            case UserCommandWord.LEFT_COMMAND: {
+                showModulesLeft(student.getModuleCodesLeft());
+                break;
+            }
+            case UserCommandWord.PACE_COMMAND: {
+                computePace(arguments, student.getCurrentModuleCredits(), student.getYear());
+                break;
+            }
+            case UserCommandWord.PREREQUISITE_COMMAND: {
+                String module = arguments[0];
+                determinePrereq(module.toUpperCase(), student.getMajor()); //to convert "CEG" to dynamic course
+                break;
+            }
+            case UserCommandWord.RECOMMEND_COMMAND: {
+                recommendScheduleToStudent(student);
+                break;
+            }
+            case UserCommandWord.ADD_MODULE_COMMAND: {
+                String module = arguments[0].toUpperCase();
+                int targetSem = Integer.parseInt(arguments[1]);
+
+                addModule(module, targetSem, student);
+                break;
+            }
+            case UserCommandWord.DELETE_MODULE_COMMAND: {
+                String module = arguments[0].toUpperCase();
+
+                deleteModule(module,student);
+                break;
+            }
+            case UserCommandWord.VIEW_SCHEDULE_COMMAND: {
+                student.printSchedule();
+                break;
+            }
+            case UserCommandWord.COMPLETE_MODULE_COMMAND: {
+                String module = arguments[0].toUpperCase();
+                //to add to user completed module
+
+                completeModule(student, module);
+
+                break;
+            }
+            case UserCommandWord.REQUIRED_MODULES_COMMAND: {
+                getRequiredModulesForStudent(student.getMajor());
+                break;
+            }
+            case UserCommandWord.INFO_COMMAND: {
+                Api.infoCommands(arguments[0], userInput);
+                break;
+            }
+            case UserCommandWord.SEARCH_MODULE_COMMAND: {
+                Api.searchCommand(userInput);
+                break;
+            }
+            case UserCommandWord.HELP_COMMAND: {
+                printListOfCommands(commandManager);
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+
+    }
+
+ */
+}
