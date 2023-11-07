d8ce88a20c30360cb1d8ccc9541d7616e455dc93
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2023-11-07 22:08:18 +0800

    add user command class to easily determine if command is valid or not

--- src/main/java/seedu/duke/models/schema/UserCommand.java
+++ src/main/java/seedu/duke/models/schema/UserCommand.java
@@ -1,20 +1,16 @@
 package seedu.duke.models.schema;
 
-import seedu.duke.models.logic.Api;
 import seedu.duke.utils.Parser;
 import seedu.duke.utils.errors.UserError;
 
-import static seedu.duke.controllers.ModuleMethodsController.*;
-import static seedu.duke.controllers.ModuleMethodsController.getRequiredModulesForStudent;
 import static seedu.duke.utils.Parser.parseArguments;
 import static seedu.duke.utils.Parser.parseCommand;
-import static seedu.duke.views.CommandLineView.printListOfCommands;
 
 public class UserCommand implements UserCommandWord {
 
     private String userInput;
     private String commandWord;
-    private static String[] arguments;
+    private String[] arguments;
     private boolean isValid;
 
     private CommandManager commandManager;
@@ -76,73 +72,5 @@ public class UserCommand implements UserCommandWord {
         }
         return this.commandWord.equals(UserCommandWord.EXIT_COMMAND);
     }
-/*
-    public void processCommand(Student student) {
-        switch (commandWord) {
-            case UserCommandWord.LEFT_COMMAND: {
-                showModulesLeft(student.getModuleCodesLeft());
-                break;
-            }
-            case UserCommandWord.PACE_COMMAND: {
-                computePace(arguments, student.getCurrentModuleCredits(), student.getYear());
-                break;
-            }
-            case UserCommandWord.PREREQUISITE_COMMAND: {
-                String module = arguments[0];
-                determinePrereq(module.toUpperCase(), student.getMajor()); //to convert "CEG" to dynamic course
-                break;
-            }
-            case UserCommandWord.RECOMMEND_COMMAND: {
-                recommendScheduleToStudent(student);
-                break;
-            }
-            case UserCommandWord.ADD_MODULE_COMMAND: {
-                String module = arguments[0].toUpperCase();
-                int targetSem = Integer.parseInt(arguments[1]);
 
-                addModule(module, targetSem, student);
-                break;
-            }
-            case UserCommandWord.DELETE_MODULE_COMMAND: {
-                String module = arguments[0].toUpperCase();
-
-                deleteModule(module,student);
-                break;
-            }
-            case UserCommandWord.VIEW_SCHEDULE_COMMAND: {
-                student.printSchedule();
-                break;
-            }
-            case UserCommandWord.COMPLETE_MODULE_COMMAND: {
-                String module = arguments[0].toUpperCase();
-                //to add to user completed module
-
-                completeModule(student, module);
-
-                break;
-            }
-            case UserCommandWord.REQUIRED_MODULES_COMMAND: {
-                getRequiredModulesForStudent(student.getMajor());
-                break;
-            }
-            case UserCommandWord.INFO_COMMAND: {
-                Api.infoCommands(arguments[0], userInput);
-                break;
-            }
-            case UserCommandWord.SEARCH_MODULE_COMMAND: {
-                Api.searchCommand(userInput);
-                break;
-            }
-            case UserCommandWord.HELP_COMMAND: {
-                printListOfCommands(commandManager);
-                break;
-            }
-            default: {
-                break;
-            }
-        }
-
-    }
-
- */
 }
