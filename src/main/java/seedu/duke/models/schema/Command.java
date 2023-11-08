b5daee7b7c71a0b9a4c862d4a86ebf5719ccebe6
SebasFok <sebasfok2001@gmail.com>
2023-11-09 00:19:34 +0800

    Make help message look nice

--- src/main/java/seedu/duke/models/schema/Command.java
+++ src/main/java/seedu/duke/models/schema/Command.java
@@ -3,8 +3,15 @@ package seedu.duke.models.schema;
 public class Command {
     private String commandName;
     private String description;
+    private String arguments;
     public Command(String commandName, String description) {
         this.commandName = commandName;
+        this.description = description;
+        this.arguments = "";
+    }
+    public Command(String commandName, String arguments, String description) {
+        this.commandName = commandName;
+        this.arguments = arguments;
         this.description = description;
     }
 
@@ -18,6 +25,7 @@ public class Command {
 
     @Override
     public String toString() {
-        return commandName + ": " + description;
+        int rightPadding = 50 - commandName.length() - arguments.length();
+        return commandName + " " + arguments + " ".repeat(rightPadding) + description;
     }
 }
