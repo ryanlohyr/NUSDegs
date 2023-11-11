ffc116c9481cd6f7cb32005cfcd50a469a29213d
rohitcube <104619746+rohitcube@users.noreply.github.com>
2023-11-11 13:40:44 +0800

    fix checkstyle

--- src/main/java/seedu/duke/controllers/ModulePlannerController.java
+++ src/main/java/seedu/duke/controllers/ModulePlannerController.java
@@ -12,7 +12,7 @@ import seedu.duke.utils.exceptions.CorruptedFileException;
 import seedu.duke.utils.exceptions.MissingFileException;
 
 import java.io.IOException;
-import java.net.ConnectException;
+//mport java.net.ConnectException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
